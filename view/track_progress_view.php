c4840c56051182d9bd1642dc3ab92d6bc729fac1
XinSing <jerry_xin92@hotmail.com>
2014-05-11 15:42:26 +0800

    form update

--- view/track_progress_view.php
+++ view/track_progress_view.php
@@ -11,221 +11,202 @@
 
 <!-- Text input-->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Age</label>  
+  <label class="col-md-4 control-label" for="Age">Age</label>  
   <div class="col-md-2">
-  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-md">
+  <input id="Age" name="Age" type="text" placeholder="" class="form-control input-md">
     
   </div>
 </div>
 
 <!-- Text input-->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Weight</label>  
+  <label class="col-md-4 control-label" for="Weight">Weight</label>  
   <div class="col-md-2">
-  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-md">
+  <input id="Weight" name="Weight" type="text" placeholder="" class="form-control input-md">
     
   </div>
 </div>
 
 <!-- Text input-->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Height</label>  
+  <label class="col-md-4 control-label" for="Height">Height</label>  
   <div class="col-md-2">
-  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-md">
+  <input id="Height" name="Height" type="text" placeholder="" class="form-control input-md">
     
   </div>
 </div>
 
-<!-- Multiple Checkboxes -->
+<!-- Multiple Radios -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="checkboxes">Today, are you feel stress?</label>
+  <label class="col-md-4 control-label" for="stress">Today, are you feel stress?</label>
   <div class="col-md-4">
-  <div class="checkbox">
-    <label for="checkboxes-0">
-      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+  <div class="radio">
+    <label for="stress-0">
+      <input type="radio" name="stress" id="stress-0" value="1" checked="checked">
       Yes
     </label>
 	</div>
-  <div class="checkbox">
-    <label for="checkboxes-1">
-      <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+  <div class="radio">
+    <label for="stress-1">
+      <input type="radio" name="stress" id="stress-1" value="2">
       No
     </label>
 	</div>
   </div>
 </div>
 
-<!-- Multiple Checkboxes -->
+<!-- Multiple Radios -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="checkboxes">Today, are you feel stress?</label>
+  <label class="col-md-4 control-label" for="happy">Today, are you feel happy?</label>
   <div class="col-md-4">
-  <div class="checkbox">
-    <label for="checkboxes-0">
-      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+  <div class="radio">
+    <label for="happy-0">
+      <input type="radio" name="happy" id="happy-0" value="1" checked="checked">
       Yes
     </label>
 	</div>
-  <div class="checkbox">
-    <label for="checkboxes-1">
-      <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+  <div class="radio">
+    <label for="happy-1">
+      <input type="radio" name="happy" id="happy-1" value="2">
       No
     </label>
 	</div>
   </div>
 </div>
 
-<!-- Multiple Checkboxes -->
+<!-- Multiple Radios -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="checkboxes">Today, are you feel happy?</label>
+  <label class="col-md-4 control-label" for="exercise">Today, do you go for exercise?</label>
   <div class="col-md-4">
-  <div class="checkbox">
-    <label for="checkboxes-0">
-      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+  <div class="radio">
+    <label for="exercise-0">
+      <input type="radio" name="exercise" id="exercise-0" value="1" checked="checked">
       Yes
     </label>
 	</div>
-  <div class="checkbox">
-    <label for="checkboxes-1">
-      <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+  <div class="radio">
+    <label for="exercise-1">
+      <input type="radio" name="exercise" id="exercise-1" value="2">
       No
     </label>
 	</div>
   </div>
 </div>
 
-<!-- Multiple Checkboxes -->
+<!-- Multiple Radios -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="checkboxes">Today, do you go for exercise?</label>
+  <label class="col-md-4 control-label" for="fruit">Today, do you eat any fruit?</label>
   <div class="col-md-4">
-  <div class="checkbox">
-    <label for="checkboxes-0">
-      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+  <div class="radio">
+    <label for="fruit-0">
+      <input type="radio" name="fruit" id="fruit-0" value="1" checked="checked">
       Yes
     </label>
 	</div>
-  <div class="checkbox">
-    <label for="checkboxes-1">
-      <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+  <div class="radio">
+    <label for="fruit-1">
+      <input type="radio" name="fruit" id="fruit-1" value="2">
       No
     </label>
 	</div>
   </div>
 </div>
 
-<!-- Multiple Checkboxes -->
+<!-- Multiple Radios -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="checkboxes">Today, do you eat any fruit?</label>
+  <label class="col-md-4 control-label" for="working">Do you go for work? If yes, normally how many hour you will spend on your working?</label>
   <div class="col-md-4">
-  <div class="checkbox">
-    <label for="checkboxes-0">
-      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
-      Yes
-    </label>
-	</div>
-  <div class="checkbox">
-    <label for="checkboxes-1">
-      <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
-      No
-    </label>
-	</div>
-  </div>
-</div>
-
-<!-- Multiple Checkboxes -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="checkboxes">Do you go for work? If yes, normally how many hour you will spend on your working?</label>
-  <div class="col-md-4">
-  <div class="checkbox">
-    <label for="checkboxes-0">
-      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+  <div class="radio">
+    <label for="working-0">
+      <input type="radio" name="working" id="working-0" value="1" checked="checked">
       1-5
     </label>
 	</div>
-  <div class="checkbox">
-    <label for="checkboxes-1">
-      <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+  <div class="radio">
+    <label for="working-1">
+      <input type="radio" name="working" id="working-1" value="2">
       6-10
     </label>
 	</div>
-  <div class="checkbox">
-    <label for="checkboxes-2">
-      <input type="checkbox" name="checkboxes" id="checkboxes-2" value="">
+  <div class="radio">
+    <label for="working-2">
+      <input type="radio" name="working" id="working-2" value="">
       &gt;10
     </label>
 	</div>
   </div>
 </div>
 
-<!-- Multiple Checkboxes -->
+<!-- Multiple Radios -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="checkboxes">Do you have the habit of eating breakfast?</label>
+  <label class="col-md-4 control-label" for="eatbreakfast">Do you have the habit of eating breakfast?</label>
   <div class="col-md-4">
-  <div class="checkbox">
-    <label for="checkboxes-0">
-      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+  <div class="radio">
+    <label for="eatbreakfast-0">
+      <input type="radio" name="eatbreakfast" id="eatbreakfast-0" value="1" checked="checked">
       Yes
     </label>
 	</div>
-  <div class="checkbox">
-    <label for="checkboxes-1">
-      <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+  <div class="radio">
+    <label for="eatbreakfast-1">
+      <input type="radio" name="eatbreakfast" id="eatbreakfast-1" value="2">
       No
     </label>
 	</div>
   </div>
 </div>
 
-<!-- Multiple Checkboxes -->
+<!-- Multiple Radios -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="checkboxes">Do you have the habit of eating supper?</label>
+  <label class="col-md-4 control-label" for="eatsupper">Do you have the habit of eating supper?</label>
   <div class="col-md-4">
-  <div class="checkbox">
-    <label for="checkboxes-0">
-      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+  <div class="radio">
+    <label for="eatsupper-0">
+      <input type="radio" name="eatsupper" id="eatsupper-0" value="1" checked="checked">
       Yes
     </label>
 	</div>
-  <div class="checkbox">
-    <label for="checkboxes-1">
-      <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+  <div class="radio">
+    <label for="eatsupper-1">
+      <input type="radio" name="eatsupper" id="eatsupper-1" value="2">
       No
     </label>
 	</div>
   </div>
 </div>
 
-<!-- Multiple Checkboxes -->
+<!-- Multiple Radios -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="checkboxes">Are you a smoker?</label>
+  <label class="col-md-4 control-label" for="smoker">Are you a smoker?</label>
   <div class="col-md-4">
-  <div class="checkbox">
-    <label for="checkboxes-0">
-      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+  <div class="radio">
+    <label for="smoker-0">
+      <input type="radio" name="smoker" id="smoker-0" value="1" checked="checked">
       Yes
     </label>
 	</div>
-  <div class="checkbox">
-    <label for="checkboxes-1">
-      <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+  <div class="radio">
+    <label for="smoker-1">
+      <input type="radio" name="smoker" id="smoker-1" value="2">
       No
     </label>
 	</div>
   </div>
 </div>
 
-<!-- Multiple Checkboxes -->
+<!-- Multiple Radios -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="checkboxes">Are you taking any alcoholic drink?</label>
+  <label class="col-md-4 control-label" for="alcoholic">Are you taking any alcoholic drink?</label>
   <div class="col-md-4">
-  <div class="checkbox">
-    <label for="checkboxes-0">
-      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+  <div class="radio">
+    <label for="alcoholic-0">
+      <input type="radio" name="alcoholic" id="alcoholic-0" value="1" checked="checked">
       Yes
     </label>
 	</div>
-  <div class="checkbox">
-    <label for="checkboxes-1">
-      <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+  <div class="radio">
+    <label for="alcoholic-1">
+      <input type="radio" name="alcoholic" id="alcoholic-1" value="2">
       No
     </label>
 	</div>
@@ -234,61 +215,62 @@
 
 <!-- Text input-->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Today, total how many distance you walk for?</label>  
+  <label class="col-md-4 control-label" for="walk">Today, how many distance you walk for?</label>  
   <div class="col-md-4">
-  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-md">
+  <input id="walk" name="walk" type="text" placeholder="" class="form-control input-md">
     
   </div>
 </div>
 
 <!-- Textarea -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textarea">Today, what do you eat for your breakfast?</label>
+  <label class="col-md-4 control-label" for="breakfast">Today, what do you eat for your breakfast?</label>
   <div class="col-md-4">                     
-    <textarea class="form-control" id="textarea" name="textarea"></textarea>
+    <textarea class="form-control" id="breakfast" name="breakfast"></textarea>
   </div>
 </div>
 
 <!-- Textarea -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textarea">Today, what do you eat for your lunch?</label>
+  <label class="col-md-4 control-label" for="lunch">Today, what do you eat for your lunch?</label>
   <div class="col-md-4">                     
-    <textarea class="form-control" id="textarea" name="textarea"></textarea>
+    <textarea class="form-control" id="lunch" name="lunch"></textarea>
   </div>
 </div>
 
 <!-- Textarea -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textarea">Today, what do you eat for your high tea?</label>
+  <label class="col-md-4 control-label" for="hightea">Today, what do you eat for your high tea?</label>
   <div class="col-md-4">                     
-    <textarea class="form-control" id="textarea" name="textarea"></textarea>
+    <textarea class="form-control" id="hightea" name="hightea"></textarea>
   </div>
 </div>
 
 <!-- Textarea -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textarea">Today, what do you eat for your dinner?</label>
+  <label class="col-md-4 control-label" for="dinner">Today, what do you eat for your dinner?</label>
   <div class="col-md-4">                     
-    <textarea class="form-control" id="textarea" name="textarea"></textarea>
+    <textarea class="form-control" id="dinner" name="dinner"></textarea>
   </div>
 </div>
 
 <!-- Textarea -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textarea">Today, what do you eat for your supper?</label>
+  <label class="col-md-4 control-label" for="supper">Today, what do you eat for your supper?</label>
   <div class="col-md-4">                     
-    <textarea class="form-control" id="textarea" name="textarea"></textarea>
+    <textarea class="form-control" id="supper" name="supper"></textarea>
   </div>
 </div>
 
 <!-- Button -->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="singlebutton"></label>
+  <label class="col-md-4 control-label" for="submit"></label>
   <div class="col-md-4">
-    <button id="singlebutton" name="singlebutton" class="btn btn-inverse">Submit</button>
+    <button id="submit" name="submit" class="btn btn-inverse">Submit</button>
   </div>
 </div>
 
 </fieldset>
 </form>
+
 
