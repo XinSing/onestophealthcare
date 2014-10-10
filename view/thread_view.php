17caddb37e469d1065d9ba201ea77266dcc266da
XinSing <jerry_xin92@hotmail.com>
2014-10-10 16:17:32 +0800

    threadview

---
+++ view/thread_view.php
@@ -0,0 +1,15 @@
+<div class='container forumblock'>
+
+	<h1 class='text-primary text-center'>Forum</h1>
+
+	
+	<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Title</h3>
+	</div>
+	<div class="panel-body">
+		Content
+	</div>
+	</div>
+
+</div>
